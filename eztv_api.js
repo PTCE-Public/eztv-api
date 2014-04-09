2193b102cb0415bdb647eb9149ec23d8fcc27089
emartin <emartin@transperfect.com>
2014-04-09 05:09:33 +0100

    URL setup

--- eztv_api.js
+++ eztv_api.js
@@ -1,9 +1,18 @@
 /*************************
-**	Variables	**
+**	Modules		**
 **************************/
 
 var request	=	require('request');
 var cheerio	=	require('cheerio');
+
+/*************************
+**	Variables	**
+**************************/
+
+var BASE_URL	=	"http://eztv.it";
+var SHOWLIST	=	"/showlist/";
+var LATEST	=	"/sort/100/";
+var SEARCH	=	"/search/";
 
 /*************************
 **	Methods		**
@@ -13,6 +22,10 @@ exports.getLatestShows	=	function() {
 };
 
 exports.getShowListing	=	function(showName) {
+
+};
+
+exports.getAllShows	=	function() {
 
 };
 
