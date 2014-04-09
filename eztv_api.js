ecb7a71829615ad9d47db0cd40d84a48cc278a64
emartin <emartin@transperfect.com>
2014-04-09 05:01:40 +0100

    Base js file

---
+++ eztv_api.js
@@ -0,0 +1,24 @@
+/*************************
+**	Variables	**
+**************************/
+
+var request	=	require('request');
+var cheerio	=	require('cheerio');
+
+/*************************
+**	Methods		**
+**************************/
+exports.getLatestShows	=	function() {
+
+};
+
+exports.getShowListing	=	function(showName) {
+
+};
+
+/*************************
+**	Objects		**
+**************************/
+
+function Listing() {
+};
