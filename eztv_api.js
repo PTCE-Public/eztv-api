fbdc75f6f569cf11e5a5bc32b69c69f2253b73e0
Eóin Martin <eoin.martin.3007@gmail.com>
2014-04-15 19:33:46 +0100

    GetLatestShows gets the show name and logs it. Basic scraping

--- eztv_api.js
+++ eztv_api.js
@@ -18,6 +18,23 @@ var SEARCH	=	"/search/";
 **	Methods		**
 **************************/
 exports.getLatestShows	=	function() {
+    request(BASE_URL + LATEST, function(error, response, html){
+
+        if(!error){
+
+            var $ = cheerio.load(html);
+
+            var title, show;
+
+            $('tr.forum_header_border[name="hover"]').filter(function(){
+                var entry = $(this);
+
+                var show = entry.children('td').first().children('a').first().children('img').first().attr('title').replace('Show Description about ', '');
+
+                console.log('Show: '+ show);
+            });
+        }
+    })
 
 };
 
